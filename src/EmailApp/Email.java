7faf555433679ec284737955f94bc926e6f47940
Nathan Burchiel <nburchiel@L-NBurchiel.syntech.org>
2022-07-26 11:40:33 -0400

    finished basic functionality

---
+++ src/EmailApp/Email.java
@@ -0,0 +1,96 @@
+package EmailApp;
+
+import java.util.Scanner;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int defaultpassLength = 8;
+    private String department;
+    private String email;
+    private int mailboxCapacity = 500;
+    private String alternateEmail;
+    private String companySuffix = "MyTechCompany.com";
+
+    // Constructor to receive first and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED : " + this.firstName + " " + this.lastName);
+
+        //call a method asking for the department - return the department
+        this.department = setDepartment();
+
+        //call a method that returns a random password
+        this.password = randomPassword(defaultpassLength);
+        System.out.println("Your password is: " + this.password);
+
+        // Combine Elements to generate email
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+        System.out.println("Your email is: " + email);
+
+    }
+
+    // Ask for department
+    private String setDepartment() {
+        System.out.println("New worker: " + firstName + "\nDEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter Department code");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) {
+            return "sales";
+        } else if (depChoice == 2) {
+            return "dev";
+        } else if (depChoice == 3) {
+            return "acct";
+        } else if (depChoice == 2) {
+            return "acct";
+        } else {
+            return "";
+        }
+    }
+        // Generate random password
+    private String randomPassword(int length) {
+            String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&";
+            char[] password = new char[length];
+            for (int i = 0; i<length; i++){
+                int rand = (int) (Math.random() * passwordSet.length());
+                password[i] = passwordSet.charAt(rand);
+            }
+            return new String(password);
+        }
+
+        // Set mailbox capacity
+    public void setMailboxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+    }
+
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
+
+    // Set Alternate email
+    public void setAlternateEmail(String altEmail){
+        this.alternateEmail = altEmail;
+    }
+
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    // Change Password
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String showInfo() {
+        return "DISPLAY NAME: " + firstName + " " + lastName + " " +
+                "\nCOMPANY EMAIL: " + email + " " +
+                "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+    }
+}
+
---
+++ src/EmailApp/EmailAppMain.java
@@ -0,0 +1,9 @@
+package EmailApp;
+
+public class EmailAppMain {
+
+    public static void main(String[] args) {
+        Email em1 = new Email("Johnny","Appleseed");
+        System.out.println(em1.showInfo());
+    }
+}
